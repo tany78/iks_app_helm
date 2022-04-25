681009e47cf8110315b551a169c8b0a048ec5ddc
tany78 <90527319+tany78@users.noreply.github.com>
2022-04-25 16:48:53 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,31 @@
+
+
+variable "ikswsname" {
+  type = string
+}
+
+resource helm_release helloiksfrtfcb {
+  name       = "helloiksapp"
+  namespace = "default"
+  chart = "https://prathjan.github.io/helm-chart/helloiks-0.1.1.tgz"
+  timeout = 600
+
+  set {
+    name  = "MESSAGE"
+    value = "Hello IKS from TFCB!!"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host = local.kube_config.clusters[0].cluster.server
+    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
+    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
+    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
+  }
+}
+
+locals {
+  kube_config = yamldecode(var.kube_harbor_config)
+}
+
